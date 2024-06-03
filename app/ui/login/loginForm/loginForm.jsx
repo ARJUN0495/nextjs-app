5152cdfb47e05e9a0f9d8ca87ba4865efd0167aa
ARJUN0495 <arjunpa95.official@gmail.com>
2024-06-03 15:12:49 +0530

    pushed new changes

--- app/ui/login/loginForm/loginForm.jsx
+++ app/ui/login/loginForm/loginForm.jsx
@@ -1,13 +1,33 @@
+import React from "react";
 import styles from "./loginForm.module.css";
 
 const LoginForm = () => {
   return (
-    <form action="/dashboard" className={styles.form}>
-      <h1>Login</h1>
-      <input type="text" placeholder="username" name="username" />
-      <input type="password" placeholder="password" name="password" />
-      <button>Login</button>
-    </form>
+    <div className={styles.container}>
+      <form action="/dashboard" className={styles.form}>
+        <h1 className={styles.title}>Login</h1>
+        <input
+          type="text"
+          placeholder="Username"
+          name="username"
+          className={styles.input}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          name="password"
+          className={styles.input}
+        />
+        <button className={styles.button}>Login</button>
+        <div className={styles.hint}>
+          <span>Hint:</span>
+          <ul>
+            <li>Username: admin</li>
+            <li>Password: admin</li>
+          </ul>
+        </div>
+      </form>
+    </div>
   );
 };
 
